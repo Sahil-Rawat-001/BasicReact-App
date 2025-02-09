092619286e0014076435775ae02c1628679d8d5e
Sahil-Rawat-001 <rawatsahilwork001@gmail.com>
2025-02-09 09:41:33 +0530

    add all files first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer(){
+
+    return(
+
+        <footer>
+            <p>
+                &copy; {new Date().getFullYear()} Dawa Daru
+            </p>
+        </footer>
+    );
+}
+
+export default Footer
