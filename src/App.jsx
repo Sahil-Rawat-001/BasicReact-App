1b3cfb7eadebaf62642a7e06653948e9144e3078
Sahil-Rawat-001 <rawatsahilwork001@gmail.com>
2025-02-18 13:32:24 +0530

    add DigitalClock comp and some images for bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import WindowSize from "./WindowSize";
+import DigitalClock from "./DigitalClock";
+
 
 function App() {
                  
@@ -6,7 +7,7 @@ function App() {
   return(
     
     <>
-      <WindowSize/>
+      <DigitalClock/>
     </>
   );
 }
